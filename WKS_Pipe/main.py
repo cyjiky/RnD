076889f922adb9185bd2cc0f13391e26d4957ce4
Viky <vikyy905@gmail.com>
2026-03-13 18:17:49 +0100

    Add example to main.py

--- WKS_Pipe/main.py
+++ WKS_Pipe/main.py
@@ -146,6 +146,9 @@ if __name__ == "__main__":
 
         url = "http://127.0.0.1:8000/coordinates"
 
+        # Example 
+        # url = "http://127.0.0.1:8000/[your endpoint URI]"
+
         mapped_serializable_dto = [
             Coordinates(
                 X=cord["X"],
